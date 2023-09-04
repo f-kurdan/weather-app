0566a81482a6c3cc1181e0c0bcf52e6325b2c7dc
Kurdanov Feytullah <fkurdan@gmail.com>
2023-09-04 21:55:48 +0300

    Add some markup

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 function SearchBar() {
     return (
-        <div>
+        <div className="searchBar">
             <form>
-                <input type="search" placeholder="Найти город" />
-                <button>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <button className="searchButton">
+                    <svg fill="none" xmlns="http://www.w3.org/2000/svg" >
                         <g id="Icon">
                             <path id="Vector" d="M14.4808 14.4808L21 21M9.5 16C13.0899 16 16 13.0899 16 9.5C16 5.91015 13.0899 3 9.5 3C5.91015 3 3 5.91015 3 9.5C3 13.0899 5.91015 16 9.5 16Z" stroke="#1B1D1F" stroke-width="2" stroke-linecap="round" />
                         </g>
                     </svg>
                 </button>
+                <input type="search" placeholder="Найти город" className="searchString"/>
             </form>
         </div>
     )
