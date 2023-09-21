02df467397813a3a9297229a4e386abf6dbfb3d0
Kurdanov Feytullah <fkurdan@gmail.com>
2023-09-21 12:41:08 +0300

    Fix bug with incorrect image display

--- src/App.js
+++ src/App.js
@@ -73,14 +73,13 @@ function App() {
         weathercode={weatherData?.current_weather?.weathercode}
         isDay={weatherData?.current_weather?.is_day} />
       <ForecastHours
-        weatherData={weatherData}
-        isDay={weatherData?.current_weather?.is_day} />
+        weatherData={weatherData} />
     </div>
   );
 }
 
 function setWeather(latitude, longitude, setWeatherData) {
-  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,weathercode&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&current_weather=true&timezone=auto&models=best_match`)
+  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,weathercode,uv_index,is_day&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto&models=best_match`)
     .then(respone => respone.json())
     .then(result => setWeatherData(result));
 }
--- src/components/ForecastHours.jsx
+++ src/components/ForecastHours.jsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { getImage } from "../services/shared";
 
-function ForecastHours({ weatherData, isDay }) {
-    let currentTime = weatherData?.current_weather?.time;
-    let timeStep = weatherData?.hourly?.time;
-    let temps = weatherData?.hourly?.temperature_2m;
-    let weathercodes = weatherData?.hourly?.weathercode;
-    const nextHourIndex = timeStep?.indexOf(currentTime) + 1;
-    const next10Hours = timeStep?.slice(nextHourIndex, nextHourIndex + 10)?.map(e => e.slice(11));
-    console.log(`time interval: ${next10Hours}`)
-    const next10HoursTemps = temps?.slice(nextHourIndex, nextHourIndex + 10)
-    console.log(next10HoursTemps)
-    const next10HoursCodes = weathercodes?.slice(nextHourIndex, nextHourIndex + 10);
+function ForecastHours({ weatherData }) {
+    const currentTime = weatherData?.current_weather?.time;
+    const timeSteps = weatherData?.hourly?.time; //часы 
+    const temps = weatherData?.hourly?.temperature_2m;//температуры
+    const weathercodes = weatherData?.hourly?.weathercode;//коды
+    const daysOrNights = weatherData?.hourly?.is_day;//день или ночь по каждому часу
 
-    let hourlyData = [];
+    const nextHourIndex = timeSteps?.indexOf(currentTime) + 1;
+    const next10Hours = timeSteps?.slice(nextHourIndex, nextHourIndex + 10)?.map(e => e.slice(11));
+    console.log(`time interval: ${next10Hours}`) //следующие 10 часов
+    const next10HoursTemps = temps?.slice(nextHourIndex, nextHourIndex + 10)//температура на следующие 10 часов
+    const next10HoursCodes = weathercodes?.slice(nextHourIndex, nextHourIndex + 10);//коды на следующие 10 часов
+    const next10HoursDayOrNight = daysOrNights?.slice(nextHourIndex, nextHourIndex + 10);//день или начь на следующие 10 часов
 
+    const hourlyData = [];
     for (let i = 0; i < next10Hours?.length; i++) {
-        hourlyData.push([next10Hours[i], next10HoursTemps[i], next10HoursCodes[i]])
+        //закидываем данные соответсвующие каждому часу в массив
+        hourlyData.push([
+            next10Hours[i], 
+            next10HoursTemps[i], 
+            next10HoursCodes[i],
+            next10HoursDayOrNight[i]])
     }
-
+    // вытаскиваем данные за каждый час по индексу
     return (
         <div className="forecast_hours">
             {hourlyData?.map(data => (
@@ -26,7 +32,7 @@ function ForecastHours({ weatherData, isDay }) {
                     <p className="forecast_hour_hour">{data[0] ?? '--'}</p>
                     <div className="forecast_hour_flexbox">
                         <p className="forecast_hour_degree">{data[1] ?? '--'}°</p>
-                        {getImage(data[2], true, isDay)}
+                        {getImage(data[2], true, data[3])}
                     </div>
                 </div>
             ))}
